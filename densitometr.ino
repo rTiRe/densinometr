568e582286126a1878885d7c364dbdce1dbf80a2
MomusWinner <areg06.06@mail.ru>
2024-05-03 01:11:18 +0300

    update arduino

--- densitometr.ino
+++ densitometr.ino
@@ -1,6 +1,3 @@
-#include <math.h>
-
-
 #define R 7
 #define G 11
 #define B 13
@@ -8,7 +5,7 @@
 #define COLORS_LEN 7
 #define RAISE_DELAY 30
 #define FALL_DELAY 40
-
+#define CONFIGURE_COD 100
 
 int colors[COLORS_LEN][3] = {{0, 0, 255}, {0, 255, 0}, {0, 255, 255}, {255, 0, 0}, {255, 0, 255}, {255, 255, 0}, {255, 255, 255}};
 float baseResistance[COLORS_LEN] = {1015.0, 1015.0, 1018, 1018.5, 1021, 1020.5, 1021.5}; // Массив для хранения базовых значений светимости
@@ -44,7 +41,14 @@ void setup() {
 
 void loop() {
   if (!Serial.available()) return;
-  iterations = Serial.readString().toInt();
+  int cod = Serial.readString().toInt();
+  if (cod == CONFIGURE_COD)
+  {
+    printBaseResistens();
+    Serial.println(1);
+    return;
+  }
+  int iterations = cod;
   if (!(iterations > 0 && iterations < 10)) return;
 
   int measurements[COLORS_LEN][iterations] = {0};
@@ -73,5 +77,34 @@ void loop() {
 
   offLight();
   Serial.println(result);
+  delay(FALL_DELAY);
+}
+
+void printBaseResistens()
+{
+  int iterations = 1;
+
+  int measurements[COLORS_LEN][iterations] = {0};
+  float avgResistance[COLORS_LEN] = {0};
+
+  for (int iteration = 0; iteration < iterations; iteration++) {
+    for (int color_id = 0; color_id < COLORS_LEN; color_id++) {
+      setLight(colors[color_id][0], colors[color_id][1], colors[color_id][2]);
+      measurements[color_id][iteration] = analogRead(P);
+      delay(RAISE_DELAY);
+      offLight();
+      delay(FALL_DELAY);
+    }
+  }
+  
+  for (int color_id = 0; color_id < COLORS_LEN; color_id++) {
+    for (int iteration = 0; iteration < iterations; iteration++) {
+      avgResistance[color_id] += measurements[color_id][iteration];
+    }
+    avgResistance[color_id] = avgResistance[color_id] / iterations;
+    // Рассчитываем значение освещенности по формуле lg(referenceResistance[color_id] / avgResistance[color_id])
+    baseResistance[color_id] = avgResistance[color_id];
+  }
+  offLight();
   delay(FALL_DELAY);
 }
